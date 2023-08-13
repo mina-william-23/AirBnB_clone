aec3bbed995f19c7d70c416757939b22440099a2
mina-william-23 <capeandroid50@gmail.com>
2023-08-13 12:40:53 -0700

    test user

--- tests/test_models/ts2_user.py
+++ tests/test_models/ts2_user.py
@@ -82,5 +82,63 @@ class TestUser_instantiation(unittest.TestCase):
         self.assertEqual(self.last_name, "alx")
 
 
+class Test_BaseModel_Save(unittest.TestCase):
+    """ test save function in BaseModel class"""
+
+    def test_save_fun(self):
+        bs = BaseModel()
+        bs_createdate = bs.created_at
+        bs_updatedate = bs.updated_at
+        self.assertEqual(bs_createdate, bs_updatedate)
+        bs.save()
+        bs_updatedate = bs.updated_at
+        self.assertNotEqual(bs_createdate, bs_updatedate)
+
+
+class Test_BaseModel_todict(unittest.TestCase):
+    """test to_dict fun in BaseModel class"""
+
+    def test_class_name(self):
+        bs = BaseModel()
+        bs_dict = bs.to_dict()
+        self.assertEqual(bs_dict['__class__'], 'BaseModel')
+
+    def test_createtime_isoformat(self):
+        bs = BaseModel()
+        bs_dict = bs.to_dict()
+        self.assertEqual(bs_dict['created_at'], bs.created_at.isoformat())
+
+    def test_updatetime_isoformat(self):
+        bs = BaseModel()
+        bs_dict = bs.to_dict()
+        self.assertEqual(bs_dict['updated_at'], bs.updated_at.isoformat())
+
+
+class Test_BaseModel_str(unittest.TestCase):
+    """test __str__ fun in BaseModel class"""
+
+    def test_class_str(self):
+        bs = BaseModel()
+        bs_class_name = bs.__class__.__name__
+        bs_dict = {key: value for key, value in bs.__dict__.items() if value}
+        str_rep = "[{}] ({}) {}".format(bs_class_name, bs.id, bs_dict)
+        self.assertEqual(str_rep, bs.__str__())
+
+    def test_class_print(self):
+        bs = BaseModel()
+        bs_class_name = bs.__class__.__name__
+        bs_dict = {key: value for key, value in bs.__dict__.items() if value}
+        str_rep = "[{}] ({}) {}\n".format(bs_class_name, bs.id, bs_dict)
+
+        capturedOutput = io.StringIO()
+        # Create StringIO object
+        sys.stdout = capturedOutput
+        #  and redirect stdout.
+        print(bs)
+        printed_str = capturedOutput.getvalue()
+        self.assertEqual(str_rep, printed_str)
+        sys.stdout = sys.__stdout__             # Reset redirect.
+
+
 if __name__ == "__main__":
     unittest.main()
